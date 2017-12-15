a6576285e3b84f821998e376dd85a76b03805549
gorouter <kenlea@126.com>
2017-12-15 13:45:26 +0800

    Update superssr.sh

--- superssr.sh
+++ superssr.sh
@@ -9,13 +9,12 @@ import time
 session = requests.session()
 user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'
 SID = ''
-
+cfduid = '__cfduid=d6252d2a32409fd4aaa5ca81bf54d4b7c1512540683; '
+distinctid = 'UM_distinctid=1602a734328bc1-07c7e841f5e638-17386d57-13c680-1602a734329bab; '
+  
 def login():
-  tm = str(int(time.time()))
-  cfduid = '__cfduid=d6252d2a32409fd4aaa5ca81bf54d4b7c1512540683; '
-  distinctid = 'UM_distinctid=1602a734328bc1-07c7e841f5e638-17386d57-13c680-1602a734329bab; '
   sid = 'sid=00f543ef193800696f40f9a897231cfc342bb7fec8a24cfcd4827cdb6cb25e7a; '
-  CNZZDATA = 'CNZZDATA1260605623=1178701526-1512540636-%7C' + tm
+  CNZZDATA = 'CNZZDATA1260605623=1178701526-1512540636-%7C' + str(int(time.time()))
   cookie = cfduid + distinctid + sid + CNZZDATA
   request_url='https://superssr.cf/auth/login'
   referer='https://superssr.cf/auth/login'
@@ -45,12 +44,8 @@ def login():
   SID = resp.cookies["sid"]
   
 def checkin():
-  tm = str(int(time.time()))
-  cfduid = '__cfduid=d6252d2a32409fd4aaa5ca81bf54d4b7c1512540683; '
-  distinctid = 'UM_distinctid=1602a734328bc1-07c7e841f5e638-17386d57-13c680-1602a734329bab; '
   sid = 'sid=' + SID + '; '
-  print sid
-  CNZZDATA = 'CNZZDATA1260605623=1178701526-1512540636-%7C' + tm
+  CNZZDATA = 'CNZZDATA1260605623=1178701526-1512540636-%7C' + str(int(time.time()))
   cookie = cfduid + distinctid + sid + CNZZDATA
   request_url='https://superssr.cf/user/checkin2'
   referer='https://superssr.cf/user'
@@ -74,6 +69,35 @@ def checkin():
   resp = session.post(request_url,data=payload,headers=headers)
   print 'CHECKIN:' + resp + resp.response
 
+ def logout():
+  sid = 'sid=' + SID + '; '
+  print sid
+  CNZZDATA = 'CNZZDATA1260605623=1178701526-1512540636-%7C' + str(int(time.time()))
+  cookie = cfduid + distinctid + sid + CNZZDATA
+  request_url='https://superssr.cf/user/logout'
+  referer='https://superssr.cf/user'
+  headers = {
+    'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'accept-encoding' : 'gzip, deflate, br',
+    'accept-language' : 'en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7',
+    'content-length' : '0',
+    'cookie' : cookie,
+    'origin' : 'https://superssr.cf',
+    'referer' : referer,
+    'user-agent' : user_agent,
+    'x-requested-with' : 'XMLHttpRequest'
+  }
+  payload = {
+    ':authority' : 'superssr.cf',
+    ':method' : 'GET',
+    ':path' : '/user/logout',
+    ':scheme' : 'https'
+  }
+  resp = session.get(request_url,data=payload,headers=headers)
+  print 'LOGOUT:' + resp + resp.response
+  
 if __name__ == '__main__':
   login()
+  time.sleep(5)
+  logout()
 
