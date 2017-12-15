af0d55145685a2c0b8cd8fcef65693f0eaf1d29a
gorouter <kenlea@126.com>
2017-12-15 13:13:02 +0800

    Update and rename supperssr.sh to superssr.sh

--- superssr.sh
+++ superssr.sh
@@ -40,7 +40,7 @@ def login():
     'remember_me' : 'week'
   }
   response= session.post(request_url,data=payload,headers=headers)
-  print response.cookie
+  print response.cookies
   
 def checkin():
   tm = str(int(time.time()))
